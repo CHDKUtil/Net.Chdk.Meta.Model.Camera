76640505891e6d1ab7803da40e2b95426fad9dfd
Dmitry Shechtman <damnian@gmail.com>
2017-07-02 01:52:08 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Model.Camera/CameraData.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Meta.Model.Camera
+{
+    public sealed class CameraData
+    {
+        public CameraModelData[] Models { get; set; }
+        public SoftwareEncodingInfo Encoding { get; set; }
+    }
+}
---
+++ src/Net.Chdk.Meta.Model.Camera/CameraModelData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Model.Camera
+{
+    public sealed class CameraModelData
+    {
+        public string[] Names { get; set; }
+        public string Platform { get; set; }
+        public Dictionary<string, VersionData> Versions { get; set; }
+        public Dictionary<string, RevisionData> Revisions { get; set; }
+    }
+}
