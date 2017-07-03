e0b56ad530d2db97c44dec8f70ac925c97a7b959
Dmitry Shechtman <damnian@gmail.com>
2017-07-03 04:16:24 +0300

    Use IDictionary<,> for Versions and Revisions

--- src/Net.Chdk.Meta.Model.Camera/CameraModelData.cs
+++ src/Net.Chdk.Meta.Model.Camera/CameraModelData.cs
@@ -6,7 +6,7 @@ namespace Net.Chdk.Meta.Model.Camera
     {
         public string[] Names { get; set; }
         public string Platform { get; set; }
-        public Dictionary<string, VersionData> Versions { get; set; }
-        public Dictionary<string, RevisionData> Revisions { get; set; }
+        public IDictionary<string, VersionData> Versions { get; set; }
+        public IDictionary<string, RevisionData> Revisions { get; set; }
     }
 }
