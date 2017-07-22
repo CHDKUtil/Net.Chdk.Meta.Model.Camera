ae65d90bfbd6cf3ae8b1a13fc2a8d8f486892cb7
Dmitry Shechtman <damnian@gmail.com>
2017-07-22 07:45:30 +0300

    Add EncodingData.Empty

--- src/Net.Chdk.Meta.Model.Camera/EncodingData.cs
+++ src/Net.Chdk.Meta.Model.Camera/EncodingData.cs
@@ -5,6 +5,11 @@ namespace Net.Chdk.Meta.Model.Camera
 {
     public sealed class EncodingData
     {
+        public static readonly EncodingData Empty = new EncodingData
+        {
+            Name = string.Empty
+        };
+
         public string Name { get; set; }
 
         [JsonConverter(typeof(HexStringJsonConverter), "x8")]
